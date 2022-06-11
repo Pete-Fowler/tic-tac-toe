e6025eb4ffcb97e0c5811c0c0205aadd418b2ef4
Pete Fowler <fowler.pete@gmail.com>
2022-06-11 16:30:01 -0600

    Add new game button click event, name prompts

--- script.js
+++ script.js
@@ -4,12 +4,33 @@ const player = (name, symbol) => {
    return {name, symbol};
 }
 
-const p1 = player('jeff', 'X'); // for testing, will need to be moved to gameplay handler
+// const p1 = player('jeff', 'X'); // for testing, will need to be moved to gameplay handler
 
 const play = (() => {
-    let currentPlayer = p1;
-    return {currentPlayer}
+
+    // function actived on click of new game button
+    //  it will 
+    const newGame = () => {
+        let p1 = prompt("What is player one's name?");
+        let p2 = prompt("what is player two's name?");
+    }
+
+    return {newGame};
+    // for testing
+    // let currentPlayer = p1;
+    // return {currentPlayer}
 })();
+
+const newGameBtn = (() => {
+    const init = () => {
+        const btn = document.getElementById("new-game");
+        console.log(btn);
+        btn.addEventListener('click', play.newGame);
+        
+    }
+    return {init};
+})();
+newGameBtn.init();
 
 const board = (() => {
     let values = ['', '', '', '', '', '', '', '', ''];

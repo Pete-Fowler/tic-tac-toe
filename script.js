a2798cee5316ede34cad3b91a4337e49d6910eb5
Pete Fowler <fowler.pete@gmail.com>
2022-06-10 20:56:11 -0600

    Add render board functionality with dummy content

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-
-
 const board = (() => {
-    init: function() {
-        let values = ['X', 'O', 'X', 'O', 'X', 'O','X', 'O'];
-    },
-    cacheDom: function () {
+    let values = ['X', 'O', 'X', 'O', 'X', 'O','X', 'O', 'X'];
+    function init() {
+        render();
+    }
+    function getBoard() { 
+        const board = Array.from(document.querySelectorAll('.cell'));
+        return board;
+    }
+    
+    function render() { 
+        let board = getBoard();
         for(let i = 0; i <= 8; i++) {
-            const board[i] = document.getElementById(`#${i}`);   
+            board[i].textContent = `${values[i]}`;
         }
-    },
-    render: function () {
-
-    },
+    }
+    return {init};
     })();
 
 const player = (name, symbol) => {
 
     return {name, symbol};
 };
+
+board.init();

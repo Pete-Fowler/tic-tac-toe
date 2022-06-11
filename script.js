57c727bc8ae90f440f97b60fad65ae2ef807adff
Pete Fowler <fowler.pete@gmail.com>
2022-06-10 18:16:01 -0600

    Add cacheDom to board module

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 
 
 const board = (() => {
+    init: function() {
+        let values = ['X', 'O', 'X', 'O', 'X', 'O','X', 'O'];
+    },
+    cacheDom: function () {
+        for(let i = 0; i <= 8; i++) {
+            const board[i] = document.getElementById(`#${i}`);   
+        }
+    },
+    render: function () {
 
-})();
+    },
+    })();
 
 const player = (name, symbol) => {
 

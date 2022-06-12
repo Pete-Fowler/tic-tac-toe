9901742e4700c64a770080853cc7a49490b7222a
Pete Fowler <fowler.pete@gmail.com>
2022-06-11 21:32:37 -0600

    Connect currentPlayer to mark()
    By moving code in play module outside of a nested function. Drafted
    switchPlayers() - works but value not moving beyond scope.

--- script.js
+++ script.js
@@ -4,39 +4,6 @@ const player = (name, symbol) => {
    return {name, symbol};
 }
 
-// const p1 = player('jeff', 'X'); // for testing, will need to be moved to gameplay handler
-
-const play = (() => {
-
-    // Handles click of New Game button
-   
-    const newGame = () => {
-        let p1Name = prompt("What is player one's name?");
-        let p2Name = prompt("what is player two's name?");
-        board.init();
-        let p1 = player(p1Name, 'X');
-        let p2 = player(p2Name, 'O');
-        let currentPlayer = p1;
-        return {p1, p2, currentPlayer};
-    }
-
-    return {newGame};
-    // for testing
-    // let currentPlayer = p1;
-    // return {currentPlayer}
-})();
-
-const newGameBtn = (() => {
-    const init = () => {
-        const btn = document.getElementById("new-game");
-        console.log(btn);
-        btn.addEventListener('click', play.newGame);
-        
-    }
-    return {init};
-})();
-newGameBtn.init();
-
 const board = (() => {
     let values = ['', '', '', '', '', '', '', '', ''];
     let cells = getBoard();
@@ -54,21 +21,47 @@ const board = (() => {
             cells[i].textContent = `${values[i]}`;
         }
     }
-
-    // bind cell event listeners
     const listen = () => {
         for(let i = 0; i <= 8; i++) {
             cells[i].addEventListener('click', mark);
         }
     }
-       // Mark with symbol on cell click
     const mark = (e) => {
         e.target.textContent = `${play.currentPlayer.symbol}`;
         values[e.target.id] = play.currentPlayer.symbol;
+        play.switchPlayers();
     }
 
     return {init};
     })();
+
+const play = (() => {
+    let p1Name = prompt("What is player one's name?");
+    let p2Name = prompt("what is player two's name?");
+    board.init();
+    
+    let p1 = player(p1Name, 'X');
+    let p2 = player(p2Name, 'O');
+    let currentPlayer = p1;
+    
+    const switchPlayers = () => {
+        if (currentPlayer === p1) {
+            currentPlayer = p2;
+        } else {
+            currentPlayer = p1;
+        }
+        return currentPlayer;
+    }
+
+    return {currentPlayer, switchPlayers};
+})();
+
+const newGameBtn = (() => {
+    const btn = document.getElementById("new-game");
+    btn.addEventListener('click', board.init);
+})();
+
+
     
   
 

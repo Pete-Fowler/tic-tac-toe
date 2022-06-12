4296a1bfc3e595a9fdfa10e2bf12bc0062842a79
Pete Fowler <fowler.pete@gmail.com>
2022-06-12 15:01:02 -0600

    Fix checkWin() logic by adding column wins

--- script.js
+++ script.js
@@ -62,14 +62,21 @@ const play = (() => {
     
     const newGame = () => {
         board.init();
-        let p1Name = prompt("What is player one's name?");
-        let p2Name = prompt("what is player two's name?");
-
-        p1 = player(p1Name, 'X');
-        p2 = player(p2Name, 'O');
+        if(p1 === undefined) {
+            setNames();
+        }
         currentPlayer = p1;
     }
    
+    const setNames = () => {
+        let p1Name = prompt("What is player one's name?");
+        let p2Name = prompt("what is player two's name?");
+        p1 = player(p1Name, 'X');
+        p2 = player(p2Name, 'O');
+        board.init();
+        currentPlayer = p1;
+    }
+
     const switchPlayers = () => {
         if (currentPlayer === p1) {
             currentPlayer = p2;
@@ -87,12 +94,18 @@ const play = (() => {
         let row1 = [board.getValues()[0], board.getValues()[1], board.getValues()[2]]; 
         let row2 = [board.getValues()[3], board.getValues()[4], board.getValues()[5]];
         let row3 = [board.getValues()[6], board.getValues()[7], board.getValues()[8]];
+        let col1 = [board.getValues()[0], board.getValues()[3], board.getValues()[6]];
+        let col2 = [board.getValues()[1], board.getValues()[4], board.getValues()[7]];
+        let col3 = [board.getValues()[2], board.getValues()[5], board.getValues()[8]];
         let diag1 = [board.getValues()[0], board.getValues()[4], board.getValues()[8]];
         let diag2 = [board.getValues()[2], board.getValues()[4], board.getValues()[6]]
 
         if ((row1.every(e => e === 'X')) || (row1.every(e => e === 'O')) || 
             (row2.every(e => e === 'X')) || (row2.every(e => e === 'O')) || 
             (row3.every(e => e === 'X')) || (row3.every(e => e === 'O')) || 
+            (col1.every(e => e === 'X')) || (col1.every(e => e === 'O')) || 
+            (col2.every(e => e === 'X')) || (col2.every(e => e === 'O')) || 
+            (col3.every(e => e === 'X')) || (col3.every(e => e === 'O')) ||
             (diag1.every(e => e === 'X')) || (diag1.every(e => e === 'O')) || 
             (diag2.every(e => e === 'X')) || (diag2.every(e => e === 'O'))) 
             {
@@ -102,16 +115,20 @@ const play = (() => {
     }
 
     const declareWinner = () => {
-        text.textContent = `${play.getPlayer().name} is the winner!`;
-        
+        text.textContent = `${play.getPlayer().name} is the winner!`; 
     }
 
-    return {newGame, getPlayer, switchPlayers, checkWin};
+    return {newGame, getPlayer, switchPlayers, checkWin, setNames};
 })();
 
 const newGameBtn = (() => {
     const btn = document.getElementById("new-game");
     btn.addEventListener('click', play.newGame);
+})();
+
+const setNamesBtn = (() => {
+    const btn = document.querySelector('#set-names');
+    btn.addEventListener('click', play.setNames);
 })();
 
 
